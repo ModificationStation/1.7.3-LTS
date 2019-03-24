665f9a58e9a6bfdba4e2b99641de7f800727c959
Dereku <dereku@default.im>
2019-03-24 20:43:48 +0700

    Restore specialsource branch

--- runtime/filehandling/srgsexport.py
+++ runtime/filehandling/srgsexport.py
@@ -71,3 +71,62 @@ def writesrgsfromcsvs(csvclass, csvmethods, csvfields, outsrgs, side):
                            '%s/%s/%s'%(row['package'], row['classname'], row['searge'])])
 
     srgshandler.writesrgs(outsrgs, {'PK':packages, 'CL':classes, 'FD':fields, 'MD':methods})
+
+def writesrgsfromcsvnames(csvclass, csvmethods, csvfields, outsrgs, side):
+    """Reads 3 CSVs and output a srgs"""
+
+    packages=[]; classes=[]; methods=[]; fields=[]
+
+    #HINT: We are adding the package conversions
+    packages.append(['.', 'net/minecraft/src'])
+    packages.append(['net', 'net'])
+    packages.append(['net/minecraft', 'net/minecraft'])
+    if side == 0:
+        packages.append(['net/minecraft/client', 'net/minecraft/client'])
+        packages.append(['net/minecraft/isom', 'net/minecraft/isom'])
+    elif side == 1:
+        packages.append(['net/minecraft/server', 'net/minecraft/server'])
+    else:
+        raise Exception("Side not recognized : %d"%side)
+
+
+    #HINT: We append the class elements. We also handle the special case of Minecraft, MinecraftApplet, MinecraftServer
+    csvreader = csv.DictReader(open(csvclass, 'r'), delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
+    for row in csvreader:
+        if int(row['side']) == side:
+            #HINT : Those checks are here to append the proper packages to notch version of the Minecraft, etc.
+            #They are needed since we don't have notch package information (lost during recompilation)
+            #The skip on start is there because of a quirk of the bot updating process
+            #We use recompiled sources, so the bot catches the Start.class which have been added by Searge.
+            if row['notch'] in ['Minecraft','MinecraftApplet']:
+                row['notch'] = 'net/minecraft/client/%s'%row['notch']
+            if row['notch'] in ['MinecraftServer']:
+                row['notch'] = 'net/minecraft/server/%s'%row['notch']
+            if row['notch'] == 'Start':continue
+            classes.append(['%s/%s'%(row['package'],row['name']), row['notch']])
+
+    #HINT: We append the method elements
+    csvreader = csv.DictReader(open(csvmethods, 'r'), delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
+    for row in csvreader:
+        if int(row['side']) == side:
+            if row['classnotch'] in ['Minecraft','MinecraftApplet']:
+                row['classnotch'] = 'net/minecraft/client/%s'%row['classnotch']
+            if row['classnotch'] in ['MinecraftServer']:
+                row['classnotch'] = 'net/minecraft/server/%s'%row['classnotch']
+            if row['classnotch'] == 'Start':continue
+            methods.append(['%s/%s/%s %s'%(row['package'], row['classname'], row['name'], row['sig']),
+                            '%s/%s %s'%(row['classnotch'],row['notch'], row['notchsig'])])
+
+    #HINT: We append the field elements
+    csvreader = csv.DictReader(open(csvfields, 'r'), delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
+    for row in csvreader:
+        if int(row['side']) == side:
+            if row['classnotch'] in ['Minecraft','MinecraftApplet']:
+                row['classnotch'] = 'net/minecraft/client/%s'%row['classnotch']
+            if row['classnotch'] in ['MinecraftServer']:
+                row['classnotch'] = 'net/minecraft/server/%s'%row['classnotch']
+            if row['classnotch'] == 'Start':continue
+            fields.append(['%s/%s/%s'%(row['package'], row['classname'], row['name']),
+                           '%s/%s'%(row['classnotch'],row['notch'])])
+
+    srgshandler.writesrgs(outsrgs, {'PK':packages, 'CL':classes, 'FD':fields, 'MD':methods})
--- runtime/reobfuscate.py
+++ runtime/reobfuscate.py
@@ -9,32 +9,38 @@ import sys, time
 from optparse import OptionParser
 from commands import Commands
 
+
 def main(conffile=None):
     commands = Commands(conffile)
 
-    commands.logger.info ('== Reobfuscating client ==')
+    commands.logger.info('== Reobfuscating client ==')
     if commands.checkbins(0):
         commands.cleanreobfdir(0)
-        commands.logger.info ('> Gathering md5 checksums')
-        commands.gathermd5s(0,True)
-        commands.logger.info ('> Compacting client bin directory')
+        commands.logger.info('> Gathering md5 checksums')
+        commands.gathermd5s(0, True)
+        commands.logger.info('> Compacting client bin directory')
         commands.packbin(0)
-        commands.logger.info ('> Reobfuscating client jar')
+        commands.logger.info('> Creating reobfuscation config')
+        commands.createsrgsforreobf(0)
+        commands.logger.info('> Reobfuscating client jar')
         commands.reobfuscate(0)
-        commands.logger.info ('> Extracting modified classes')
+        commands.logger.info('> Extracting modified classes')
         commands.unpackreobfclasses(0)
 
-    commands.logger.info ('== Reobfuscating server ==')
+    commands.logger.info('== Reobfuscating server ==')
     if commands.checkbins(1):
         commands.cleanreobfdir(1)
-        commands.logger.info ('> Gathering md5 checksums')
-        commands.gathermd5s(1,True)
-        commands.logger.info ('> Compacting server bin directory')
+        commands.logger.info('> Gathering md5 checksums')
+        commands.gathermd5s(1, True)
+        commands.logger.info('> Compacting server bin directory')
         commands.packbin(1)
-        commands.logger.info ('> Reobfuscating server jar')
+        commands.logger.info('> Creating reobfuscation config')
+        commands.createsrgsforreobf(1)
+        commands.logger.info('> Reobfuscating server jar')
         commands.reobfuscate(1)
-        commands.logger.info ('> Extracting modified classes')
+        commands.logger.info('> Extracting modified classes')
         commands.unpackreobfclasses(1)
+
 
 if __name__ == '__main__':
     parser = OptionParser(version='MCP %s' % Commands.MCPVersion)
